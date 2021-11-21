095cfcc9a41aad64ac6985c36333fcc782050c4a
Quirly <MataAtari@gmx.de>
2021-11-21 09:25:45 +0100

    project pre-finalization

--- README.md
+++ README.md
@@ -11,3 +11,23 @@ The starter project has some HTML and CSS styling to display a static version of
 To get started, open `js/app.js` and start building out the app's functionality
 
 For specific, detailed instructions, look at the project instructions in the Udacity Classroom.
+
+## Project Goal
+
+As this project refers to a landing page we have to tackle flexibility. We never know how many sections will be published as this can vary. Thus we have to ensure that - despite of this fact - the navigation bar will contain the
+correct items and is reconstructed everytime the DOM changes. Moreover the functionality of the navigation bar has to
+be ensured. If the user clicks on the respective section, the page shall be scrolled smoothly to the section clicked.
+If a section is visible in the viewport, this section shall be highlighted in the navigation bar. The page shall be
+responsive.
+
+## Project Approach
+
+As it is best practices to separate styling (css), structure (html) and functionality (javascript), I have added the
+app.js-file to the index.html file. I put it on the end to ensure, that every element is rendered and interpreted before code will be applied to it. Event listening will only start if the DOM is loaded which is ensured by the
+"DOMContentLoaded" event listener in app.js.
+
+
+## User Manual
+
+- Simply add or delete new sections dynamically in the index.html
+
--- js/app.js
+++ js/app.js
@@ -11,24 +11,29 @@
  * 
  * JS Standard: ESlint
  * 
-*/
+/** Global Variables: Sections and Number of Sections */
 let sections = Array.from(document.getElementsByTagName("section"));
+let no_sections = sections.length;
 /**
 * Begin Helper Functions
 */
-/** Remove classes if a section is clicked in navigation bar */
+/** Helper 1: Removes "your-active-class" if user scrolls before adding the visible one again */
 function help_remove_classes() {
     sections.forEach(function (section) { section.classList.remove("your-active-class") });
 }
 
-function addClass() {
+/** Helper 2: Calls isInViewport function to check if an element is visible. Adds "your-active-class" if so.*/
+function function_check_viewport_add_class() {
+    help_remove_classes();
     sections.forEach(function (section) {
-        if (isInViewport(section)) {
+        let x = isInViewport(section);
+        if (x == true) {
             section.classList.add("your-active-class");
         }
     });
 }
 
+/** Helper 3: Checks if an element is visible. Returns True if so.*/
 function isInViewport(element) {
     const rect = element.getBoundingClientRect();
     return (
@@ -39,41 +44,52 @@ function isInViewport(element) {
     );
 }
 
+
 /**
-* End Helper Function
+* End Helper Functions
 */
 
 /**
 * Begin Main Function
 */
-function function_udacity_landing_short() {
-    /**
-    * Set navigation bar as variable ul
-    */
-    const ul = document.getElementById("navbar__list");
 
-    let sections = Array.from(document.getElementsByTagName("section"));
-    let no_sections = sections.length;
+function function_udacity_landing_short() {
+
+    /** Set navigation bar as variable ul */
+    const ul = document.getElementById("navbar__list");
 
     /** Create list element as first item in navigation bar left, start with MAIN */
     const i = 0;
 
+    /** Create sections step by step, append them to navigation bar and add Click EventListener for scrolling */
     sections.forEach(function (section, i) {
         let li = document.createElement("li");
         li.appendChild(document.createTextNode(section.id));
         ul.appendChild(li);
-        ul.childNodes[i].addEventListener('click', function () { section.scrollIntoView({ behavior: 'smooth' }); help_remove_classes(), false });
+        ul.childNodes[i].addEventListener('click', function () { section.scrollIntoView({ behavior: 'smooth' }), false });
         i++;
         let new_end = i;
     })
 
+    /** Add Main item to navigation bar to allow the user to scroll to top anytime*/
     let li = document.createElement("li");
     li.appendChild(document.createTextNode("Main"));
-    ul.appendChild(li).addEventListener('click', function () { window.scrollTo({ top: 0, behavior: 'smooth' }); help_remove_classes(), false });
+    ul.appendChild(li).addEventListener('click', function () { window.scrollTo({ top: 0, behavior: 'smooth' }), false });
 }
 
+/**
+* End Main Function
+*/
 
+/**
+* Begin Event Listeners
+*/
 
 /** If DOM is ready, the navigation bar is build by the main function dynamically; event listeners are added*/
 document.addEventListener('DOMContentLoaded', function_udacity_landing_short);
-document.addEventListener('scroll', addClass());
+/** If the user scrolls, it shall be checked which section is in the viewport to highlight it in the navigation bar */
+document.addEventListener('scroll', function_check_viewport_add_class);
+
+/**
+* End Event Listeners
+*/
